d95d29da8fd2d0d41ecc75c59024fdf52043de24
qiyazhong <qiyazhong@163.com>
2018-09-19 16:51:26 +0800

    delete all applicationContext-*.xml file

--- tc-web/src/main/java/com/yz/tc/Application.java
+++ tc-web/src/main/java/com/yz/tc/Application.java
@@ -2,13 +2,8 @@ package com.yz.tc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
-import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication(exclude = {SessionAutoConfiguration.class, DataSourceAutoConfiguration.class, RedisAutoConfiguration.class})
-@ImportResource({"classpath:spring/applicationContext-*.xml"})
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
